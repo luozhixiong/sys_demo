1cb760b3f8356eab57983160a283d15771e577c8
luozhixiong <luozhixiong666@163.com>
2015-04-23 16:53:37 +0800

    f

---
+++ sys/src/main/java/org/lzx/sys/dao/UserDao.java
@@ -0,0 +1,13 @@
+package org.lzx.sys.dao;
+
+import org.lzx.sys.annotation.MyBatisDao;
+import org.lzx.sys.model.User;
+
+/**
+ * 用户DAO接口
+ */
+@MyBatisDao
+public interface UserDao extends BaseDao<User>{
+
+	
+}
